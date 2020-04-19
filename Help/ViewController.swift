2c935257556897d2061e64eadc1af85d98557d19
Adam <billparker149@gmail.com>
2020-04-18 14:25:08 -1000

    Initial Commit

---
+++ Help/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Help
+//
+//  Created by Daniel Lei on 4/18/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
