419503c0b82852255a3fd6137bed07d69412496f
Adam <billparker149@gmail.com>
2020-04-23 14:37:00 -1000

    Profile Page test 1

---
+++ Help/ProfileViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileViewController.swift
+//  Help
+//
+//  Created by Daniel Lei on 4/23/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var phonenumberLabel: UILabel!
+    
+    @IBOutlet weak var ECnameLabel: UILabel!
+    
+    @IBOutlet weak var ECphonenumber: UILabel!
+    
+    @IBOutlet weak var bloodtypeLabel: UILabel!
+    
+    @IBOutlet weak var allergyLabel: UILabel!
+    
+    @IBOutlet weak var othermedicalLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
